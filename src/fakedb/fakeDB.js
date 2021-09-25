64d3b8abbfd5d191c908574a52e3711922f5acdd
AnnomsDev <foysalbinnour@gmail.com>
2021-09-26 02:04:47 +0600

    a short fake DB added

---
+++ src/fakedb/fakeDB.js
@@ -0,0 +1,152 @@
+let db = [
+    {
+        key: 1,
+        name: 'Yen-Thanh Le',
+        username: '@yenthanh132',
+        country: 'United States',
+        education: 'Ho Chi Minh City University of Natural Sciences, Ho Chi Minh City',
+        badges: 'problemSolving',
+        salary: 2508,
+        img: 'https://hrcdn.net/s3_pub/hr-avatars/ce538d93-74c4-412c-99d3-acf58bfdd914/150x150.png'
+    },
+    {
+        key: 2,
+        name: 'Shahed Shahriar',
+        username: '@Z0RRO',
+        country: 'Bangladesh',
+        education: 'University of Dhaka',
+        badges: 'problemSolving, Python, Sql ',
+        salary: 2548,
+        img: 'https://hrcdn.net/s3_pub/hr-avatars/d4dfd888-b5dc-424b-8c41-68445adc6844/150x150.png'
+    },
+    {
+        key: 3,
+        name: 'Ke Shen',
+        username: '@xiper',
+        country: 'China',
+        education: 'University of Electronic Science and Technology of China',
+        badges: 'problemSolving, Java, Python, Sql',
+        salary: 2842,
+        img: 'https://hrcdn.net/s3_pub/hr-avatars/30c13c87-f5a6-4be7-8671-e7e6f2c46634/150x150.png'
+    },
+    {
+        key: 4,
+        name: 'Alexey Zayakin',
+        username: '@Alex_2oo8',
+        country: 'Latvia',
+        education: 'University of Latvia, Rīga',
+        badges: 'problemSolving',
+        salary: 2754,
+        img: 'https://hrcdn.net/s3_pub/hr-avatars/5d32e4e3-fd45-442e-8cfd-386d82a1daad/150x150.png'
+    },
+    {
+        key: 5,
+        name: 'Roberto Sales',
+        username: '@rsalesc',
+        country: 'Brazil',
+        education: 'Universidade Federal da Bahia',
+        badges: 'problemSolving',
+        salary: 2505,
+        img: 'https://secure.gravatar.com/avatar/14e7faf56e67b96e2f82850fa59eef6e?d=https://d1ce3iv5vajdy.cloudfront.net/hackerrank/assets/gravatar.jpg&s=150'
+    },
+    {
+        key: 6,
+        name: 'Ivan Kazmenko',
+        username: '@Gassa',
+        country: 'Russian Federation',
+        education: 'St. Petersburg State University',
+        badges: 'problemSolving, Sql',
+        salary: 2504,
+        img: 'https://secure.gravatar.com/avatar/b09d4a96700cad15ebfa201e6b8c89df?d=https://d1ce3iv5vajdy.cloudfront.net/hackerrank/assets/gravatar.jpg&s=150'
+    },
+    {
+        key: 7,
+        name: 'Joshua Simmons',
+        username: '@kadoban',
+        country: 'United States',
+        education: 'Rochester Institute if Technology,Rochester',
+        badges: 'problemSolving',
+        salary: 2500,
+        img: 'https://secure.gravatar.com/avatar/6acf272cf32a76cf4fc26a5e292c03de?d=https://d1ce3iv5vajdy.cloudfront.net/hackerrank/assets/gravatar.jpg&s=150'
+    },
+    {
+        key: 8,
+        name: 'Vernon Gutierrez',
+        username: '@verngutz',
+        country: 'Philippines',
+        education: 'Ateneo de Manila University',
+        badges: 'problemSolving',
+        salary: 2497,
+        img: 'https://hrcdn.net/s3_pub/hr-avatars/b186126c-b198-4cd8-9b0f-e5ebbad706ba/150x150.png'
+    },
+    {
+        key: 9,
+        name: '',
+        username: '',
+        country: '',
+        education: '',
+        badges: 'problemSolving',
+        salary: 0,
+        img: ''
+    },
+    {
+        key: 10,
+        name: '',
+        username: '',
+        country: '',
+        education: '',
+        badges: 'problemSolving',
+        salary: 0,
+        img: ''
+    },
+    {
+        key: 11,
+        name: '',
+        username: '',
+        country: '',
+        education: '',
+        badges: 'problemSolving',
+        salary: 0,
+        img: ''
+    },
+    {
+        key: 12,
+        name: '',
+        username: '',
+        country: '',
+        education: '',
+        badges: 'problemSolving',
+        salary: 0,
+        img: ''
+    },
+    {
+        key: 13,
+        name: '',
+        username: '',
+        country: '',
+        education: '',
+        badges: 'problemSolving',
+        salary: 0,
+        img: ''
+    },
+    {
+        key: 14,
+        name: '',
+        username: '',
+        country: '',
+        education: '',
+        badges: 'problemSolving',
+        salary: 0,
+        img: ''
+    },
+    {
+        key: 15,
+        name: '',
+        username: '',
+        country: '',
+        education: '',
+        badges: 'problemSolving',
+        salary: 0,
+        img: ''
+    },
+]
