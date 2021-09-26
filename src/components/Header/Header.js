2bf1fbf823166da5de92ce56ce30dca3a9e85f5c
AnnomsDev <foysalbinnour@gmail.com>
2021-09-26 15:05:28 +0600

    Header modefied little bit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div className='header'>
             <h1>Create your Own Clan of <span>P</span>rogrammers</h1>
             <p>A community of peoples, Who start counting with Zero :)</p>
-            <h1>Total Xp limit: 10000</h1>
+            <h3>Total Xp limit: 10000</h3>
         </div>
     );
 };
