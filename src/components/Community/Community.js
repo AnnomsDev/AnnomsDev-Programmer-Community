9a811232c8c6f7d0d9582c040d89f530fd292c8b
AnnomsDev <foysalbinnour@gmail.com>
2021-09-26 06:48:19 +0600

    community section implemented

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Clan from '../Clan/Clan';
+import Programmer from '../Programmer/Programmer';
 import './Community.css'
 
 const Community = () => {
+    const [programmers, setProgrammers] = useState([])
+
+    useEffect(() => {
+        fetch('./fakeDB.json')
+            .then(res => res.json())
+            .then(data => setProgrammers(data))
+    }, [])
+
+
+
     return (
         <div className='community'>
             <div className='programmer-container'>
                 {/* app programmers will be added here */}
+                {
+                    programmers.map(programmer => <Programmer
+                        key={programmer.key}
+                        programmer={programmer}
+                    ></Programmer>)
+                }
+
             </div>
             {/* clan: All selected programmer will be added here like cart */}
             <Clan></Clan>
--- src/components/Programmer/Programmer.js
+++ src/components/Programmer/Programmer.js
@@ -8,20 +8,23 @@ const badgeIcon = <FontAwesomeIcon className='icon' icon={faAward} />
 const gloveIcon = <FontAwesomeIcon className='icon' icon={faGlobe} />
 const xpIcon = <FontAwesomeIcon className='icon' icon={faRadiation} />
 
-const Programmer = () => {
+const Programmer = (props) => {
+    const { name, username, country, education, badges, xp, img } = props.programmer
+    console.log(props.programmer)
+
     return (
         <div className='programmer'>
             {/* <h1>from Programmer</h1> */}
             <div className="top-container">
-                <img src="https://hrcdn.net/s3_pub/hr-avatars/99bf9b2b-3024-4976-8d35-479622e33bd9/150x150.png" alt="" />
-                <h2>RD Rashal</h2>
-                <p>@afasdf</p>
+                <img src={img} alt="" />
+                <h2>{name}</h2>
+                <p>{username}</p>
             </div>
             <div className="details">
-                {eduIcon}<p><b>Education: </b> ITMO University</p>
-                {badgeIcon}<p><b>Badge: </b>problemSolving, Python, Sql </p>
-                {gloveIcon}<p><b>Country: </b> Bangladesh</p>
-                {xpIcon}<p><b>XP: </b> 4055</p>
+                {eduIcon}<p><b>Education: </b> {education} </p>
+                {badgeIcon}<p><b>Badge: </b>{badges}</p>
+                {gloveIcon}<p><b>Country: </b>{country}</p>
+                {xpIcon}<p><b>XP: </b>{xp}</p>
             </div>
             <button>Add To Clan</button>
 
